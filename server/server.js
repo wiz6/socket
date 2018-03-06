d756ff1f008640f80f1123f915997f38f582989c
Mart Madisson <mart.madisson@delfi.ee>
2018-03-06 12:45:47 +0200

    init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+let app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+	res.send('index.html');
+});
+
+app.listen(3000, () => {
+	console.log(`Server is up in port ${port}`);
+});
