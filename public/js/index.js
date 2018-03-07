b91eea9b3bca362fd9cf675c7a24b40c04a449e2
Mart Madisson <mart.madisson@delfi.ee>
2018-03-07 11:46:44 +0200

    scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 let socket = io();
 
+function scrollToBottom () {
+	// Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child')
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function() {
 	console.log('Connected to server');
 });
@@ -16,6 +32,7 @@ socket.on('newMessage', function(message) {
 		text: message.text
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -27,6 +44,7 @@ socket.on('newLocationMessage', function(message) {
 		url: message.url
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
