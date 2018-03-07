b19053aff32b76511c8ce4d484ed1fef16a2d513
Mart Madisson <mart.madisson@delfi.ee>
2018-03-07 14:53:50 +0200

    send messageto only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -71,7 +71,7 @@ jQuery('#message-form').on('submit', function(e) {
 	var messageTextbox = jQuery('[name=message]');
 
 	socket.emit('createMessage', {
-		from: 'User', text: messageTextbox.val()
+		text: messageTextbox.val()
 	}, function() {
 		messageTextbox.val('')
 	});
